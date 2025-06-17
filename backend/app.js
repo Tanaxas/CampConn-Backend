0306389153d9d46659d50f621a8c14c4a49fb60d
Tanaxas <154374874+Tanaxas@users.noreply.github.com>
2025-06-17 19:10:48 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -80,7 +80,7 @@ app.use((err, req, res, next) => {
     error: process.env.NODE_ENV === 'development' ? err.message : undefined
   });
 });
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT; //|| 10000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
