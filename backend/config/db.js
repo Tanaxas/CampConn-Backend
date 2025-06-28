efa8ff4c4c795b338cc0da192c0cda73791e8221
Tanaxas <154374874+Tanaxas@users.noreply.github.com>
2025-06-28 21:20:43 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  timezone: 'Z',
   waitForConnections: true,
   connectionLimit: 30,
   queueLimit: 0
